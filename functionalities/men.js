dc086bf66dfaf96b7c10ca288de426b58b42bdee
pauloh349 <muiruripaul3492@gmail.com>
2025-03-24 23:32:09 +0300

    shopit

--- functionalities/men.js
+++ functionalities/men.js
@@ -1,4 +1,5 @@
 import {
+  getFirestore,
   collection,
   query,
   where,
@@ -7,7 +8,21 @@ import {
   getDoc,
 } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-firestore.js";
 
-import { db } from "../config/config";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-app.js";
+
+
+ const firebaseConfig = {
+      apiKey: "AIzaSyCLpDUGI56SZbY6NXfVhI7-1N4iVBEqg5s",
+      authDomain: "shopit-c9343.firebaseapp.com",
+      projectId: "shopit-c9343",
+      storageBucket: "shopit-c9343.firebasestorage.app",
+      messagingSenderId: "1047611691322",
+      appId: "1:1047611691322:web:ef0738ee3d5ea320954003",
+      measurementId: "G-T6BZZ45YB2"
+    };
+
+    const app = initializeApp(firebaseConfig);
+    const db = getFirestore(app);
 
 // Function to fetch products by category
 async function fetchProductsByCategory() {
